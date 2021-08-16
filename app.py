39a6a8a1c503f758d91da4cefa3d347084e1a7c7
get2sriram <get2sriram@gmail.com>
2021-08-16 08:43:06 +0530

    Updates

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-from flask import Flask, jsonify,  request, render_template
-import joblib
-import pandas as pd
+## import the needed libraries
+import json
+from flask import Flask, request, render_template
 from model import *
 
+## define the app
 app = Flask(__name__)
 
+## API to send the home page to the browser
 @app.route('/')
 def home():
     return render_template('index.html')
 
+## API to process the input from the web browser when "Predict" is clicked.
+## If no username is provided to predict, render the home page again to the browser
+## If username is provided to predict, validate the username using the validate_user function.
+## If valid user then get the recommendations, format to JSON and send back to the browser for display
+## Else send a message to provide a valid user
 @app.route("/predict", methods=['POST'])
 def predict():
     if (request.method == 'POST'):
+
         user_name = [x for x in request.form.values()]
         print(user_name)
-        final_recomm = recommendation(user_name)
-        graphJSON0 = final_recomm.to_json(orient='records')
-        return render_template('index.html',graphJSON0=graphJSON0)
+
+        user_name = user_name[0]
+        if validate_user(user_name):
+            final_recomm = recommendation(user_name)
+            graphJSON0 = final_recomm.to_json(orient='records')
+        else:
+            graphJSON0 = json.dumps([{"name":"Enter a valid user to recommend top products"}])
+
+        return render_template('index.html', graphJSON0=graphJSON0)
     else :
         return render_template('index.html')
 
--- model.py
+++ model.py
@@ -1,12 +1,27 @@
+## import the need libraries
+import pickle
+import pandas as pd
+
+## load the recommendation and sentiment classification model
+recomm_model_load = pd.read_pickle("./models/recommendation.pkl")
+sentiment_model_load = pd.read_pickle("./models/sentiment_model.pkl")
+
+## function to validate if the input user is valid
+def validate_user(usr_input):
+    global recomm_model_load
+    if usr_input in list(recomm_model_load.index):
+        return True
+    else:
+        return False
+
+## function to take the userid as input, get the top 20 recommendations for the user using the recommendation model,
+## filter the top 5 products based on sentiment using the sentiment model. Please note the sentiment model prediction
+## is done ahead of time and the %Positive sentiment is calculated.  This is used as the sentiment model to avoid
+## processing a large number of reviews during runtime and application failures due to memory issues in heroku.
 def recommendation(usr_input):
-    import pickle
-    import pandas as pd
-    from sklearn.feature_extraction.text import TfidfVectorizer
+    global recomm_model_load, sentiment_model_load
 
-    recomm_model_load = pd.read_pickle("./models/recommendation.pkl")
-    sentiment_model_load = pd.read_pickle("./models/sentiment_model.pkl")
-
-    usr_recomm = recomm_model_load.loc[usr_input].iloc[0].sort_values(ascending=False)[0:20]
+    usr_recomm = recomm_model_load.loc[usr_input].sort_values(ascending=False)[0:20]
     usr_recomm = pd.DataFrame(usr_recomm)
     usr_recomm = usr_recomm.reset_index()
     final_recomm = \
